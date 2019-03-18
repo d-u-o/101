483e402f72bc34928d5d19541556877b5aa7aa40
Tim Menzies <tim.menzies@gmail.com>
2019-03-17 22:50:14 -0400

    saving

--- src/rows.lua
+++ src/rows.lua
@@ -47,7 +47,7 @@ use 'src/sym.lua'
 -- - `Data` may have one (and only) one `class` column.
 
 function data(header)
-  return {class=nil,  show=nil, name=header, xyz={}, nums={}, syms={},
+  return {class=nil,  show=nil, names={}, name=header, xyz={}, nums={}, syms={},
           x={}, y={},z={},  rows={} } 
 end
 
@@ -59,18 +59,14 @@ function null(_)      return {} end
 function nullAdd(t,x) return x end
 function nullSub(t,x) return x end
 
--- ## Making `data`
--- ### Step1: `header`
+local function cell(r,val,what,pos,add)
+  if val ~= "?" then
+    val = tonumber(val) or val
+    r[what][pos] = val
+    add( about, val) end 
+end
 
--- The function `header` reads and processes special symbols
--- that define a table.
---
--- - '<' is a dependent goal to be maximized (it is also numeric);
--- - '>' is a dependent goal to be minimized (it is also numeric);
--- - '$' is an independent  numeric colum;
--- - '!' is a class column (and is not numeric).
-
-local function meta(t,txt,    pos,what,add,about)
+local function column(t,c,txt,    pos,what,add,about,tmp)
   what = "x"
   if txt:match(":")      then what = "z" end
   if txt:match("[<>%!]") then what = "y" end
@@ -82,103 +78,45 @@ local function meta(t,txt,    pos,what,add,about)
          if what == "x" then t.nums[pos] = pos end
     else about,add = sym(),symAdd
          if what == "x" then t.syms[pos] = pos end
-    end
-  end
-  t[what][put] = {
-         txt=    txt, 
-         what=   what,
-         pos=    pos,
-         nump=   add == numAdd,
-         about=  about, 
-         classp= txt:match('!') ~= nil,
-         add=    function (z,r)
-                   if z ~= "?" then
-                     z = tonumber(z) or z
-                     r[what][pos] = z
-                     add( about, z) end end}
-  if txt:match("!") then t.classp = t[what][pos] end
-  return t[what][put] 
+  end end
+  tmp= {
+    txt=    txt, 
+    pos=    pos,
+    what=   what,
+    about=  about, 
+    nump=   add == numAdd,
+    classp= txt:match('!') ~= nil,
+    add=    function (r,z) cell(r,z,what,pos,add) end
+  }
+  if txt:match("!") then t.classp = tmp end
+  t[what][put]   = tmp
+  t.nameHas[txt] = tmp
+  t.has[c]       = tmp
 end
 
-function header(cells,t,       c,w)
-  if t.show then show(cells) end
-  t = t or data(cells))
-  t.name=cells
-  for c,x in pairs(cells) do
-    if not x:match("%?")  then
-      t.xyz[c] = meta(t,x) end end
+function header(cells,d,       c,w)
+  if d.show then show(cells) end
+  d = t or data(cells))
+  d.header=cells
+  for c,txt in pairs(cells) do
+    if not x:match("%?")  then column(d,c,txt) end end
   return t
 end
-
--- For example, the above example call to `header` initializes
--- a `data` with the following structure. 
--- Note that columns 2 and 3
---    have each been given a [`num`](num) object or
---    a [`sym`](sym) object (we will use those to
---    collect statistics on each column). 
--- Also, observe how
---    column three has a weight of `w[3]==-1`.
-
---
---        _use:
---        |  1: 1
---        |  2: 2
---        |  3: 3
---        |  4: 4
---        |  5: 5
---        class: 5
---        name:
---        |  1: outlook
---        |  2: $temp
---        |  3: <humid
---        |  4: wind
---        |  5: !play
---        nums:
---        |  2:  <an empyy num>
---        |  3:  <an empty num>
---        rows:
---        syms:
---        |  1:  <an empty sym>
---        |  4:  <an empty sym>
---        |  5:  <an empty sym>
---        w:
---        |  3: -1
---
-
--- Another thing to observe is that the `rows` and `syms`
--- and `nums` are empty.
--- Why? Because we have yet to add any data to this `data`.
--- That task is handled by `row`.
-
-
--- ### Step2: Add a `row`
--- The only tricky bits of adding a row is handling the 
--- string to number conversions, and when to skip
--- cells with an unknown value.
-
 
 function rowAdd(t,cells,     r,val)
   if t.show then show(cells)  end
   r= row()
-  for c,xyz in pairs(t.xyz) do
-    xyz.add(cells[c], r) end
+  for c,xyz in pairs(t.xyz) do xyz.add(r, cells[c]) end
   t.rows[ #t.rows + 1 ] = r
   return t
 end  
 
 function clone(data0, rows,   data1)
-   data1 = header(data0.name)
+   data1 = header(data0.header)
    for _,cells in pairs(rows or data0.rows) do 
      rowAdd(data1, cells) end
    return data1
 end
-
--- ## Making `data` from Ram 
---
--- Reading data from disk, is handled by the
--- `rows` function (that sets some defaults), after
--- which time it calls `rows1` to do the actually
--- stream over the disk data. 
 
 function rows1(stream, t,f0,f,   first,line,cells)
   first,line = true,io.read()
@@ -199,18 +137,3 @@ function rows(file,t,f0,f,      stream,txt,cells,r,line)
   return rows1( file and io.input(file) -- reading from some specified file
                       or io.input(),    -- reading from standard input
                 t  or data(), f0 or header, f or rowAdd) end 
-
--- ## Making `data` from Ram 
--- Note that if your data
--- was in RAM, you would **not** use `rows`. Rather,
--- your code would:
---
--- - would call `header` to create a `data`,
--- - then call `row` for each row of data.
---
--- For example, to select all rows from `old` whose last cells is `happy`...
---   
---     new = header(old.names)
---     for _,cells in pairs(old.rows) do
---        if cells[#cells] == 'happy' then
---            row(new, cells) end end
