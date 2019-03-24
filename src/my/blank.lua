93642e779d57539d35bdfa4665df27fc372ca45c
Tim Menzies <tim.menzies@gmail.com>
2019-03-24 01:27:55 -0400

    saving

--- src/my/blank.lua
+++ src/my/blank.lua
@@ -1,6 +1,7 @@
 -- vim: ft=lua ts=2 sw=2 sts=2 et : cindent : formatoptions+=cro
 -- Duo101  copyright (c) 2018,2019 Tim Menzies, timm@ieee.org 
 -- All rights reserved, opensource.org/licenses/BSD-3-Clause
+-- For examples of usage, see demos (at end).
 --------- --------- --------- --------- --------- --------- ---------
 
 local Object=require('use')('src/lib.lua')
